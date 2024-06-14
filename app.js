2e72b21ce8beb02f23791636b97d6eae0aa64b02
regmipreety <priety.regmi46@gmail.com>
2024-06-14 09:55:42 -0500

    Adding bootstrap and partial views

--- app.js
+++ app.js
@@ -9,16 +9,27 @@ app.set('view engine', 'ejs')
 //listen for requests 
 app.listen(3000);
 
+//adding bootstrap
+app.use(
+    "/",express.static("./node_modules/bootstrap/dist")
+  );
+
 app.get('/', (req,res)=> {
     //res.send('<p> home page </p')
    // res.sendFile('./views/index.html', { root: __dirname });
-   res.render('index')
+   const blogs = [
+    {title: 'Lorem ipsum', snippet: 'The classic latin passage that just never gets old, enjoy as much'},
+    {title: 'Hodor Ipsum', snippet: 'If you havent seen Game of Thrones, go watch it right now'},
+    {title: 'Trump Ipsum', snippet: 'If you havent seen Game of Thrones, go watch it right now'},
+
+   ];
+   res.render('index', {title : 'Home', blogs})
    //express will look inside the views folder 
 })
 
 app.get('/about', (req,res)=> {
    // res.sendFile('./views/about.html', {root: __dirname});
-   res.render('about')
+   res.render('about', {title: 'About'})
 })
 
 //redirects
@@ -28,5 +39,5 @@ app.get('/about-us', (req, res)=>{
 
 //404
 app.use((req,res)=>{
-    res.status(404).render('404')
+    res.status(404).render('404', {title: '404 Not Found'})
 })
---
+++ views/README.md
@@ -0,0 +1,2 @@
+ EJS Templates
+* EJS templates are processed through the EJS view engine on the server
