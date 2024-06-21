fcfb133ec70cdc9b26028dddc6007d67eac36f17
regmipreety <priety.regmi46@gmail.com>
2024-06-20 20:36:05 -0500

    Adding sign up and log in functionality

---
+++ Documentation.md
@@ -0,0 +1,8 @@
+Promise
+* The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value
+Await
+* The await operator is used to wait for a Promise. It can only be used inside an async function within regular JavaScript code; however it can be used on it own with JS modules
+
+Async/Await: It allows you to write asynchronous code in a synchronous manner. Async doesn't have resolve or reject parameters. Everything after Await is placed in an event queue
+* Async makes  function return a promise
+* Await makes an async function wait for a promise
--- controllers/authController.js
+++ controllers/authController.js
@@ -4,33 +4,46 @@ const signup_get = (req, res)=>{
     res.render('login/signup', {title: 'Sign up'})
 }
 
-const signup_post = (req, res)=>{
+const signup_post = async (req, res)=> {
     const { email, password } = req.body;
    // const user = new User(req.body)
-    
-    //Validate if username and password are provided
+
     if (!email || !password) {
         return res.status(400).send("Email and password are required");
     }
-
-   // Create a new User instance with Mongoose
-    const user = new User({ email , password });
+    try {
+          // Create a new User instance with Mongoose
+        const user = new User({ email , password });
     
-    // Save the user to the database
-    user.save()
-        .then((result)=> {
-            res.status(201).render('home', {title:"Welcome"})
-        })
-        .catch((err)=>{
-            console.log(err);
-            res.status(500).send("Failed to create user");
-        });
+        // Save the user to the database
+       const data = await user.save()
+        res.status(201).render('home', {title:"Welcome"})
+            
+    } 
+    catch (err){
+        console.log(err)
+        res.status(500).send("Failed to create user");
+    }
 }
+
 const login_get = (req, res)=>{
-    res.render('login/signup', {title: 'Log in'})
+    res.render('login/login', {title: 'Log in'})
 }
-const login_post = (req, res)=>{
-    res.render('login/signup', {title: 'Log in'})
+const login_post = async (req, res)=>{
+    const { email, password } = req.body;
+    // const user = new User(req.body)
+ 
+     if (!email || !password) {
+         return res.status(400).send("Email and password are required");
+     }
+     try {
+           const logged = await User.find({email: email, password:password})
+           res.status(201).render('home', {title:"Welcome", users:logged})         
+     } 
+     catch (err){
+         console.log(err)
+         res.status(500).send("Incorrect email/password");
+     }
 }
 
 module.exports = {
