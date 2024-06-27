4249c8136f379c9188174281a212673b30984c5c
regmipreety <priety.regmi46@gmail.com>
2024-06-27 10:39:30 -0500

    setting cookie for jwt during sign up

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 //handle errors
 const handleErrors = (err) => {
@@ -8,6 +9,7 @@ const handleErrors = (err) => {
         errors.email = 'Email is already registered'
         return errors 
     }
+    console.log(err)
     //validation errors
     if(err._message.includes('User validation failed')){
         Object.values(err.errors).forEach(properties => {
@@ -15,6 +17,12 @@ const handleErrors = (err) => {
         })
     }
     return errors;
+}
+const maxAge = 3 * 24 * 60 * 60 //seconds in 3 days
+const createToken = (id)=>{
+    return jwt.sign({ id }, 'my secret key', {
+        expiresIn: maxAge
+    })
 }
 
 const signup_get = (req, res)=>{
@@ -28,10 +36,12 @@ const signup_post = async (req, res)=> {
     try {
           // Create a new User instance with Mongoose
         const user = await User.create({ email , password });
+        const token = createToken(user._id)
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000})
     
         // Save the user to the database
       // const data = await user.save()
-        res.status(201).render('home', {title:"Welcome"})
+        res.status(201).json({user: user._id});
             
     } 
     catch (err){
