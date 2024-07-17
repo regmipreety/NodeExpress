0cce36a796194af9db8d6f38f58a0fc136c314fb
regmipreety <priety.regmi46@gmail.com>
2024-07-17 12:36:35 -0500

    server

--- server.js
+++ server.js
@@ -1,47 +1,11 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-const _ = require('lodash');
-const path = require('path')
-const dotenv = require('dotenv')
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
 
-dotenv.config({ path:'./config.env'})
-
-const server = http.createServer((req,res)=>{
-    //lodash
-
-    // const num = _.random(0, 20);
-    // console.log(num);
-
-
-//set header content type
-// res.setHeader('Content-Type', 'text/html');
-// let path = './views/';
-// switch(req.url) {
-//     case '/':
-//         path += 'index.html';
-//         res.statusCode = 200;
-//         break;
-    
-//     default: 
-//         path += '404.html';
-//         res,statusCode = 400;
-//         break;
-//}
-
-//send a html file
-
-fs.readFile(path, (err, data)=>{
-    if(err){
-        console.log(err)
-    }else {
-        res.write(data);
-        res.end()
-    }
-})
-})
-const port = process.env.PORT || 3000
-server.listen(port, 'localhost', ()=>{
-    console.log('listening to server')
-})
-
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
